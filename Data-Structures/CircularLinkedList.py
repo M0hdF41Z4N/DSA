d258c736b33760ce04478119c45b6047eb976e06
M0hdF41Z4N <31098815+M0hdF41Z4N@users.noreply.github.com>
2022-09-22 11:17:00 +0530

    Create CircularLinkedList.py

---
+++ Data-Structures/CircularLinkedList.py
@@ -0,0 +1,16 @@
+# Implementation of Circular Linked List
+# Using Singly Linked List
+
+class Node:
+  def __init__(self,val):
+    self.data = val
+    self.next = none
+    
+    
+    
+class CircularLinkedList:
+  def __init__(self):
+    self.tail = None
+    
+  def addFront(self,key):
+    pass
