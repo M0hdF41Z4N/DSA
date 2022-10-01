c0990415a647cf75cc67720a24046933073dafdb
mfaiza1808 <mfaiza1808@DESKTOP-27EASP3>
2022-10-01 12:19:41 +0530

    Counting Sort

---
+++ Algorithms/CountingSort.py
@@ -0,0 +1,37 @@
+# Implementation of Counting Sort
+
+
+def countingSort(nums):
+    m = max(nums) # finding maximum in array 
+    n = len(nums)
+    # Initialize count array
+    count = [0] * (m+1)
+    output = [0] * n
+
+    # Store the count of each elements in count array
+    for i in range(n):
+        count[nums[i]] += 1
+
+    # Store the cummulative count
+    for i in range(1,len(count)):
+        count[i] += count[i-1]
+
+    i = n-1
+    while i >= 0:
+        output[count[nums[i]] - 1] =  nums[i]
+        count[nums[i]] -= 1
+        i -= 1
+
+    # copy the output in array
+    for i in range(len(output)):
+        nums[i] = output[i]
+
+
+
+
+# Driver Code
+
+numbers = [9,8,6,2,3,5,6,12]
+print("Before Soritng -> ",numbers)
+countingSort(numbers)
+print("After Sorting -> ",numbers)
