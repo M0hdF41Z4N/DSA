6c395810e08f0a1a1f328056f6386d14b92f118c
M0hdF41Z4N <31098815+M0hdF41Z4N@users.noreply.github.com>
2022-12-22 08:11:15 +0530

    Update BinaryTree.py

--- Data-Structures/BinaryTree.py
+++ Data-Structures/BinaryTree.py
@@ -1,2 +1,179 @@
-# Implementation of 
-# Binary Tree
+#   Binary Tree Implementation
+
+class Node:
+    # Constructor
+    def __init__(self,val):
+        self.data = val
+        self.left = self.right = None
+    
+class binary_tree:
+    # Constructor
+    def __init__(self):
+        self.root = None
+        self.nodeCount = 0
+
+    def height(self):
+        pass
+
+    def build_using_in_pre_order(self,inOrder,inStart,inEnd,preOrder,preStart,preEnd):
+        if (preStart>preEnd or inStart>inEnd):
+            return None
+
+		# Assigning root
+        root = Node(preOrder[preStart])
+		
+		# Parting using root
+        k = 0
+        for i in range(inStart,inEnd+1):
+            if (inOrder[i] == root.data):
+                k = i
+                break
+
+		# Attaching left and right subtree
+        root.left = self.build_using_in_pre_order(preOrder, preStart+1 , preStart + (k-inStart) , inOrder , inStart ,k-1)
+
+        root.right = self.build_using_in_pre_order(preOrder,preStart+(k-inStart)+1,preEnd,inOrder,k+1,inEnd)
+        return root
+    
+    def build_using_in_post_order(self,inOrder,inStart,inEnd,postOrder,postStart,postEnd):
+        if (postStart>postEnd or inStart>inEnd):
+            return None
+
+		# Assigning root
+        root = Node(postOrder[postEnd])
+		
+		# Parting using root
+        k = 0
+        for i in range(inStart,inEnd+1):
+            if (inOrder[i] == root.data):
+                k = i
+                break
+
+		# Attaching left and right subtree
+        root.left = self.build_using_in_post_order(postOrder, postStart , postStart + (k-inStart-1) , inOrder , inStart ,k-1)
+
+        root.right = self.build_using_in_post_order(postOrder,postStart+(k-inStart),postEnd-1,inOrder,k+1,inEnd)
+        return root
+
+    def build(self,inOrder,preOrder):
+        inStart = preStart = 0
+        inEnd = preEnd = len(inOrder)
+        self.root = self.build_using_in_pre_order(inOrder,inStart,inEnd,preOrder,preStart,preEnd)
+
+    # Function to print binary search tree
+    # in Pre-Order Traversal
+    def preOrder(self):
+        return self.__preOrder(self.root)
+
+    def __preOrder(self,node):
+        Order = []
+        if node:
+            Order.append(node.data)
+            Order+=self.__preOrder(node.left)
+            Order+=self.__preOrder(node.right)
+        return Order
+    
+
+    
+    # Iterative Method
+    #def preOrderTraversal(self):
+    #    stack = [self.root]
+    #    Order = []
+    #    while stack:
+    #        node = stack.pop()
+    #        Order.append(node.data)
+    #        if node.right :
+    #            stack.append(node.right)
+    #        if node.left :
+    #            stack.append(node.left)
+    #    return Order
+   
+
+    # Function to print binary search tree
+    # in Post-Order Traversal
+    def postOrder(self):
+        return self.__postOrder(self.root)
+
+    
+    # Recursive approach
+
+    def __postOrder(self,node):
+    # code here
+        Order = []
+        if node != None:
+            Order+=self.__postOrder(node.left)
+            Order+=self.__postOrder(node.right)
+            Order.append(node.data)
+        return Order
+
+    # Function to print binary search tree
+    # in In-Order Traversal
+    def inOrder(self):
+        return self.__inOrder(self.root)
+
+
+    
+    # Recursive Approach
+    def __inOrder(self,node):
+        Order = []
+        if node:
+            Order+=self.__inOrder(node.left)
+            Order.append(node.data)
+            Order+=self.__inOrder(node.right)
+        return Order
+    
+
+    """
+    # Iterative Method
+    def inOrderTraversal(self):
+        stack = []
+        Order = []
+        curr = self.root
+        while True:
+            if curr:
+                stack.append(curr)
+                curr = curr.left
+            elif stack:
+                curr = stack.pop()
+                Order.append(curr.data)
+                curr = curr.right
+            else:
+                break
+        return Order
+    """
+
+    # Function to print binary search tree
+    # in Level-Order Traversal
+    def levelOrder(self):
+        return self.__levelOrder(self.root)
+
+    def __levelOrder(self,root):
+        # Iterative Method
+        Order = []
+        queue = [root]
+        while queue:
+            node = queue.pop(0)
+            Order.append(node.data)
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+        return Order
+
+    def print(self):
+        return self.__print(self.root)
+    
+    def __print(self,root):
+        Order = []
+        queue = [root]
+        while queue:
+            node = queue.pop(0)
+            print(node.data,": ",end="",sep="")
+            Order.append(node.data)
+            if node.left:
+                print("L:{0},".format(node.left.data),end="")
+                queue.append(node.left)
+            if node.right:
+                print("R:{0}".format(node.right.data),end="")
+                queue.append(node.right)
+            print("\n")
